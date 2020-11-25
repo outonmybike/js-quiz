c28d84202b532214a2e086e1d807de5778b24d94
DAN NELSON <dannelson@DANs-Air.lan>
2020-11-24 23:11:37 -0700

    more formatting and function enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,15 +20,17 @@ var questionsArray =[
 ]
 
 var challengeRulesContent = 'Here are the rules of the challenge enter as many as you want Lucas ipsum dolor sit amet qui-gonn leia darth luke vader fett cade jinn hutt naboo. Lucas ipsum dolor sit amet qui-gonn leia darth luke vader fett cade jinn hutt naboo.'
-var mainFieldEl = document.querySelector('#main-field');
+var mainFieldEl = document.querySelector('#main');
+var feedbackDiv = document.querySelector('#feedback')
 var display = document.querySelector('#time');
 
-
-
-
-
-
-
+var viewresultsPage = function() {
+	var prevScreen = document.querySelector('section')
+	prevScreen.remove()
+	var questionScreen = document.createElement('section');
+	questionScreen.innerHTML='<h2>All done</h2><h3>Your final score is:';
+	mainFieldEl.appendChild(questionScreen)
+}
 
 
 var i = 0;	
@@ -38,75 +40,78 @@ var beginGame = function() {
 	return
 }
 
-var postQuestionHandler = function () {
-	console.log('now what??')
-}
 
 var nextQuestion = function() {
-		var prevScreen = document.querySelector('section')
-		prevScreen.remove()
-		var questionScreen = document.createElement('section');
-		questionScreen.innerHTML='<h1>'+questionsArray[i].title+'</h1>';
-		var answerDiv = document.createElement('div');
-		answerDiv.className = 'answer-container';
-		questionScreen.appendChild(answerDiv);
-		var feedbackDiv = document.createElement('div');
-		feedbackDiv.className = 'feedback-container';
-		questionScreen.appendChild(feedbackDiv);
-		for(x=0; x<questionsArray[i].responses.length; x++) {
-			var responseDiv = document.createElement('div')
-			responseDiv.className = 'response-container'
-			answerDiv.appendChild(responseDiv)
-			var responseButton = document.createElement('button');
-			responseButton.textContent = questionsArray[i].responses[x];
-			responseButton.className='response-button'
-			responseDiv.appendChild(responseButton);
-		}
-		mainFieldEl.appendChild(questionScreen);
+	var prevScreen = document.querySelector('section')
+	prevScreen.remove()
+	
+	var questionScreen = document.createElement('section');
+	questionScreen.innerHTML='<h2>'+questionsArray[i].title+'</h2>';
+	var answerDiv = document.createElement('div');
+	answerDiv.className = 'answer-container';
+	questionScreen.appendChild(answerDiv);
+	mainFieldEl.appendChild(questionScreen);
 
-		var responseSelected = function(event) {
-			questionScreen.removeEventListener('click',responseSelected);
-			var clickedItem = event.target.closest('.response-button')
-			if(!clickedItem) {
-				return false;
-			}
-			var feedbackReply = document.createElement('div')
-			feedbackDiv.appendChild(feedbackReply);
-			if(clickedItem.textContent===questionsArray[i].correct) {
-				feedbackReply.textContent='Correct'
-			}
-			else {
-				feedbackReply.textContent='Wrong'
-			}
-			var postQuestionButton = document.createElement('button')
-			feedbackDiv.appendChild(postQuestionButton)
-			if (i === questionsArray - 1) {
-				postQuestionButton.className = 'results-button'
-				postQuestionButton.texContent = 'View Results'
-			}
-			else {
-				postQuestionButton.className = 'next-question-button'
-				postQuestionButton.texContent = 'Next Question'
-			}
-			postQuestionButton.addEventListener('click',postQuestionHandler)
-		}
-		questionScreen.addEventListener('click',responseSelected)
-};
+	for(x=0; x<questionsArray[i].responses.length; x++) {
+		var responseDiv = document.createElement('div')
+		responseDiv.className = 'response-container'
+		answerDiv.appendChild(responseDiv)
+		var responseButton = document.createElement('button');
+		responseButton.textContent = questionsArray[i].responses[x];
+		responseButton.className='response-button'
+		responseDiv.appendChild(responseButton);
+	}
+	questionScreen.addEventListener('click',responseSelected)
+	return
+}
+
+
+var responseSelected = function(event) {
+	var prevFeedback = document.querySelector('.feedback-container')
+	if(prevFeedback) {
+		prevFeedback.remove();
+	}
+	var feedBackTextBox = document.createElement('div');
+	feedBackTextBox.className = 'feedback-container';
+	feedbackDiv.appendChild(feedBackTextBox);
+
+	var clickedItem = event.target.closest('.response-button')
+	if(!clickedItem) {
+		return false;
+	}
+	if(clickedItem.textContent===questionsArray[i].correct) {
+		feedBackTextBox.innerHTML='<h3>Correct!</h3>'
+	}
+	else {
+		feedBackTextBox.innerHTML='<h3>Wrong!</h3>'
+	}
+	i++;
+	if(i===questionsArray.length) {
+		viewresultsPage();
+		return
+	}
+	else {
+		nextQuestion()
+		return
+	}
+	return
+	}
+
 
 
 
 var landingPage = function() {
 	var startScreenEl = document.createElement('section');
 	startScreenEl.className = 'landing-page';
-	startScreenEl.id = 'start-up';
 	startScreenEl.innerHTML = '<h1>Coding Quiz Challenge</h1><p>'+challengeRulesContent+'</p>'
 	var startButtonEl = document.createElement('button');
 	startButtonEl.className = 'start-button';
-	startButtonEl.textContent = 'Start Quiz'
-	startScreenEl.appendChild(startButtonEl)
+	startButtonEl.textContent = 'Start Quiz';
+	startScreenEl.appendChild(startButtonEl);
 	mainFieldEl.appendChild(startScreenEl);
 
 	startButtonEl.addEventListener('click',beginGame);
+	return
 };
 
 landingPage();
